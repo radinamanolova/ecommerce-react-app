5d72a7b4a58ebd2a4757492158ddd61f090823b3
Radina Manolova <radina.manolova96@gmail.com>
2021-02-18 11:54:09 +0200

    updating about us page

--- client/src/pages/about/about.component.jsx
+++ client/src/pages/about/about.component.jsx
@@ -7,6 +7,7 @@ const AboutUsPage = () => (
     <p>This is a demo e-commerce solution and its purpose is non-commercial.</p>
     <p>The sign in and sign up options are only for the purpose of testing the project.</p>
     <p>The payment option is again only for testing and there is a demo credit card information for that purpose.</p>
+    <p>All images used on the site are from <a href="https://unsplash.com/" target="_blank" rel="noreferrer">Unsplash</a>.</p>
   </div>
 );
 
